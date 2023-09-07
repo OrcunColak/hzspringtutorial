f10fabd87fc4f451c101c916c35482ee4c6cb349
Orçun Çolak <orcun.colak@hazelcast.com>
2023-09-07 11:08:08 +0300

    Initial commit

--- src/main/java/com/colak/hzspringtutorial/HzSpringTutorialApplication.java
+++ src/main/java/com/colak/hzspringtutorial/HzSpringTutorialApplication.java
@@ -22,13 +22,10 @@ public class HzSpringTutorialApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-
         IMap<Long, String> map = hazelcastInstance.getMap("data");
         for (long i = 0; i < 10; i++) {
             String value = map.get(i);
             logger.info("Map value : {} ", value);
         }
-
-
     }
 }
